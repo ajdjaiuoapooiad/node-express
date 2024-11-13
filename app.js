9c3b824a9425d090b451b8aa8da689987944461a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-13 17:36:37 +0900

    53ch ok

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-const fs = require('fs');
 
 
-// app.use(express.json());
+app.use(express.json());
 
 
 // app.get('/',(req,res) => {
@@ -31,9 +31,25 @@ app.get('/api/v1/tours',(req,res) => {
 
 
 app.post('/api/v1/tours',(req,res) => {
-    console.log(req.body);
-    res.send('Done');
-    
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId},req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    newTour
+                }
+            });           
+        }
+    );
 });
 
 
