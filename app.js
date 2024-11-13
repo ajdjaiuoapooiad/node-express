8c0c679ebeff06538a8e0328e8d3e857e75bc317
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-13 14:35:38 +0900

    52ch ok

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 
-app.get('/',(req,res) => {
-    res.status(200).send('Hello from the server')
-})
+// app.get('/',(req,res) => {
+//     res.status(200).send('Hello from the server')
+// })
+
+
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+
+
+app.get('/api/v1/tours',(req,res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    });
+});
+
+
 
 const port = 3000;
 app.listen(port, () => {
