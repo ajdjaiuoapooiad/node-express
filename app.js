67db56a557cde44017b65daf191d18190f3bf6be
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-14 14:57:13 +0900

    error

--- app.js
+++ app.js
@@ -31,10 +31,7 @@ app.use((req,res,next) => {
 // 3) ROUTES
 
 app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/user',userRouter);
+app.use('/api/v1/users',userRouter);
 
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,115 +1,19 @@
 const express = require('express');
-
+const userController = require('./../controllers/userController');
 
 const router = express.Router();
 
 
-
-
-const getAllUsers = (req,res) => {
-    console.log(req.requestTime);
-    
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-};
-
-
-const getUser = (req,res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-
-
-    if (!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-    
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
-};
-
-
-
-const createUser = (req,res) => {
-    // console.log(req.body);
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId},req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    newTour
-                }
-            });           
-        }
-    );
-};
-
-
-const updateUser = (req,res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated>'
-        }
-    })
-};
-
-const deleteUser = (req,res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<deleted>'
-        }
-    })
-};
-
-
-
 router
-.route('/')
-.get(getAllUsers)
-.post(createUser);
+.route('/') 
+.get(userController.getAllUsers)
+.post(userController.createUser);
 
 router
 .route('/:id')
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser);
+.get(userController.getUser)
+.patch(userController.updateUser)
+.delete(userController.deleteUser);
 
 
 
