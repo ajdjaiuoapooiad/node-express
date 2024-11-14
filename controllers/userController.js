44c6f144eadbddcb4943afa5741709eef228c22c
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-14 15:16:00 +0900

    errro

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,94 +1,31 @@
-
-
-exports.getAllUsers = (req,res) => {
-    console.log(req.requestTime);
-    
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
     });
-};
-
-
-exports.getUser = (req,res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-
-
-    if (!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-    
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
+  };
+  exports.getUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
     });
-};
-
-
-
-exports.createUser = (req,res) => {
-    // console.log(req.body);
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId},req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    newTour
-                }
-            });           
-        }
-    );
-};
-
-
-exports.updateUser = (req,res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated>'
-        }
-    })
-};
-
-exports.deleteUser = (req,res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'error'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<deleted>'
-        }
-    })
-};
-
+  };
+  exports.createUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  };
+  exports.updateUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  };
+  exports.deleteUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined!'
+    });
+  };
+  
