3608e43b05ea9df93fbf1c1d71b2f5ae536be3b5
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-14 16:20:17 +0900

    checkBody ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,6 +19,16 @@ exports.checkID = (req,res,next,val) => {
     next();
 };
 
+exports.checkBody = (req,res,next) => {
+    if (!req.body.name || !req.body.price){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'missing name'
+        });
+    }
+    next();
+};
+
 
 
 exports.getAllTours = (req,res) => {
